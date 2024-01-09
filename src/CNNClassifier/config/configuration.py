f37f71a9b8d3dc87e2754caad3147ef635f932bf
shuva41 <machineautoshuva41@gmail.com>
2024-01-09 21:06:01 +0530

    Miscellaneous experiments are done

---
+++ src/CNNClassifier/config/configuration.py
@@ -0,0 +1,11 @@
+from CNNClassifier.utils.utils import read_yaml, create_directory
+from CNNClassifier.entity.config_entity import DataIngestionConfig
+from CNNClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
+from pathlib import Path
+import os
+
+class ConfigurationManager:
+    def __init__(self):
+        pass
+    def get_data_ingestion_config(self):
+        pass
